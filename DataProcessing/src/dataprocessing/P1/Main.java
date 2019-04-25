e72f0dc7c9f4683f688741a9871dd57eff36877d
Tip <tip.klaassen@student.hu.nl>
2019-04-25 18:16:02 +0200

    P1 fix

--- DataProcessing/src/dataprocessing/P1/Main.java
+++ DataProcessing/src/dataprocessing/P1/Main.java
@@ -5,33 +5,33 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] arg) {
-		ReizigerOracleDaoImpl odao = new ReizigerOracleDaoImpl();
+		ReizigerOracleDaoImpl reizigerDB = new ReizigerOracleDaoImpl();
 		Reiziger r1 = new Reiziger("Tip", Date.valueOf("1997-04-16"), 1);
 		Reiziger r2 = new Reiziger("Saar", Date.valueOf("1997-05-20"), 2);
 		
 		//Opslaan van reizigers naar ArrayList
-		odao.save(r1);
-		odao.save(r2);
+		reizigerDB.save(r1);
+		reizigerDB.save(r2);
 
 		//Ophalen en uitprinten van alle reizigers
-		List<Reiziger> lijst = odao.findAll();
+		List<Reiziger> lijst = reizigerDB.findAll();
 		for(Reiziger r : lijst) {
 			System.out.println(String.format("Naam: %s, Geboortedatum: %s, ID: %s",r.getNaam(),r.getGbdatum(),r.getID()));
 		}
 		System.out.println();
 		
 		//Ophalen en uitprinten van alle reizigers met een bepaalde geboortedatum
-		List<Reiziger> lijst2 = odao.findByGbdatum("1997-04-16");
+		List<Reiziger> lijst2 = reizigerDB.findByGbdatum("1997-04-16");
 		for(Reiziger r : lijst2) {
 			System.out.println(String.format("Naam: %s, Geboortedatum: %s, ID: %s",r.getNaam(),r.getGbdatum(),r.getID()));
 		}
 		System.out.println();
 		
 		//Verwijderen van reiziger uit ArrayList
-		odao.delete(r1);
+		reizigerDB.delete(r1);
 		
 		//Opnieuw ophalen en uitprinten van alle reizigers
-		List<Reiziger> lijst3 = odao.findAll();
+		List<Reiziger> lijst3 = reizigerDB.findAll();
 		for(Reiziger r : lijst3) {
 			System.out.println(String.format("Naam: %s, Geboortedatum: %s, ID: %s",r.getNaam(),r.getGbdatum(),r.getID()));
 		}
@@ -39,10 +39,10 @@ public class Main {
 		
 		//Updaten van reiziger
 		r2.setGbdatum(Date.valueOf("1998-05-20"));
-		odao.update(r2);
+		reizigerDB.update(r2);
 		
 		//Opnieuw ophalen en uitprinten van alle reizigers
-		List<Reiziger> lijst4 = odao.findAll();
+		List<Reiziger> lijst4 = reizigerDB.findAll();
 		for(Reiziger r : lijst4) {
 			System.out.println(String.format("Naam: %s, Geboortedatum: %s, ID: %s",r.getNaam(),r.getGbdatum(),r.getID()));
 		}
